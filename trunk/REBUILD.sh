b6494e189676056bf38aec7b5d20a51ef7108fa6
tomas.hanak <tomas.hanak@d58a301a-c8c5-0f9b-2d22-4c34cb17f32c>
2010-11-13 22:33:33 +0000

    Rewritten rebuild script to compile all sources into one folder.

--- trunk/REBUILD.sh
+++ trunk/REBUILD.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-make distclean 2> /dev/null
+./CLEAN.sh
 
-qmake && time make
+BUILD_DIR="_build_"
+MAKE_ARGS="-j 5"
+
+OLD_DIR=`pwd`
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+qmake ../QtCommunication.pro -o Makefile.QtCommunication && \
+    make $MAKE_ARGS -f Makefile.QtCommunication && \
+    qmake ../tests/console/test.pro -o Makefile.test && \
+    make $MAKE_ARGS -f Makefile.test
+
+cd $OLD_DIR
